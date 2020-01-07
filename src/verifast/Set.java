61fd0a7bc42513642d3a2b4e9fef057bf6728d07
pedrocarrega <pedro.carrega@outlook.com>
2020-01-07 20:03:45 +0000

    missing unique verfication/add

--- src/verifast/Set.java
+++ src/verifast/Set.java
@@ -1,141 +1,109 @@
 
 public class Set{
+	
 	/*@
-	predicate set() =
-		this.size |-> ?s &*&
-		this.arraySize |-> ?a &*&
+	predicate set(list<Object> elems) =
 		this.elements |-> ?e &*&
-		e[0..a] |-> ?elems &*&
-		a == length(elems);
-	@*/
+    		this.size |-> ?s &*&
+    		e[0..s] |-> elems &*& // get hold of the elems in the stack
+    		e[s..e.length] |-> _ &*&
+    		s == length(elems); // the invariant
+ 	@*/
+	
 
 
 private Object[] elements;
-private int arraySize;
 private int size;
 private static final int DEFAULT_CAPACITY = 10;
 
     public Set() 
         //@ requires true;
-        //@ ensures set();
-    {    elements = new Object[DEFAULT_CAPACITY];
-    	arraySize = DEFAULT_CAPACITY;
+        //@ ensures set(nil);
+    {   
+     	elements = new Object[DEFAULT_CAPACITY];
+    	size = 0;
     }
 
+
     public boolean isEmpty() 
-        //@ requires set() &*& size |-> ?s;
-        //@ ensures set() &*& result == (s == 0);
+        //@ requires set(?elems) &*& size |-> ?s;
+        //@ ensures set(elems) &*& result == (s == 0);
     {    return size == 0;
     }
 
     public int size() 
-        //@ requires set() &*& size |-> ?s;
-        //@ ensures set() &*& result == s;
+        //@ requires set(?elems) &*& size |-> ?s;
+        //@ ensures set(elems) &*& result == s;
     {    return size;
     }
 
 
     public void clear()
-    	//@ requires set() &*& elements |-> ?e &*& arraySize |-> ?a &*& size |-> ?s &*& s > 0;
-    	//@ ensures set() &*& s == 0;
+    	//@ requires set(?elems) &*& elements |-> ?e &*& size |-> ?s &*& s > 0;
+    	//@ ensures set(elems) &*& s == 0;
     {
-        //@open set();
-        for(int i = 0; i < arraySize; i++)
-        //@ invariant array_slice(elements, 0, arraySize, _) &*& 0 <= i;
+        //@open set(elems);
+        for(int i = 0; i < size; i++)
+        //@ invariant array_slice(elements, 0, size, _) &*& 0 <= i;
         {
         	elements[i] = null;
         }
         size = 0;
-        //@close set();
+        //@close set(elems);
     }
 
 
     public boolean contains(Object o)
-        //@ requires set() &*& arraySize |-> ?a;
-        //@ ensures set();
+        //@ requires set(?elems) &*& size |-> ?s;
+        //@ ensures set(elems);
         {
-        //@ open set();
-        for(int i = 0; i < arraySize; i++)
-        //@ invariant array_slice(elements, 0, arraySize, _) &*& 0 <= i;
+        //@ open set(elems);
+        for(int i = 0; i < size; i++)
+        //@ invariant array_slice(elements, 0, size, _) &*& 0 <= i;
         {
         	if(elements[i] == e){
         		return true;
         	}
         }
-        //@close set();
+        //@close set(elems);
         
         return false;
     }
     
+
     public void add(Object o)
-    	//@ requires set() &*& arraySize |-> ?a &*& size |-> ?s &*& s != a;
-    	//@ ensures set();
-    {
-    	boolean unique = true;
-    	int index = 0;
-    	//@ open set();
-    	for(int i = 0; unique && i < arraySize; i++)
-    	//@ invariant array_slice(elements, 0, arraySize, _) &*& 0 <= i;
-    	{
-    		if(elements[i] == o){
-    			unique = false;
-    		}
-    		
-    		if(elements[i] == null){
-    			index = i;
-    		}
-   	 }
-   	 if(unique){
-   	 	elements[index] = o;
-   	 }
-   	 //@ close set();
-    }
-    
-/*
-    public void add(Object o)
-        //@ requires set();// &*& !contains(o);
-        //@ ensures set();//(append(elems, cons(o, nil)));
+        //@ requires set(?elems) &*& elements |-> ?e;
+        //@ ensures set(elems) &*& set(append(elems, cons(o, nil)));
         {
         if(size == elements.length){
-        	Object[] newElements = new Object [size * 2 + 1];
-        	//@ close array_slice_dynamic(array_slice_Object, elements, 0, arraySize, _);
-        	//@ close array_slice_dynamic(array_slice_Object, newElements, 0, arraySize, _);
-        	//@ close arraycopy_pre(array_slice_Object, false, 1, elements, 0, arraySize, _, newElements, 0);
-      		System.arraycopy(elements, 0, newElements, 0, arraySize);
-        	//@ open arraycopy_post(_, _, _, _, _, _, _, _, _);
-        	//@ open array_slice_dynamic(array_slice_Object, newElements, _, _, _);
-      		elements = newElements;
-      		elements[size++] = o;
-      		arraySize = size * 2 + 1;	
-        }else{
-        	for(int i = 0; i< elements.length; i++)
-        	//@ invariant array_slice(elements, 0, elements.length, _) &*& 0 <= i;
-        	{
-        		if(elements[i] == null){
-        			elements[i] = o;
-        			break;
-        		}
-        	}
+        	Object[] newElements = new Object[size*2+1];
+        	System.arraycopy(elements, 0, newElements, 0, size);
+        	elements = newElements;
         }
-        size++;
+        elements[size++] = o;
     }
-    */
+    
 
     public void remove(Object o)
-        //@ requires set() &*& size |-> ?s &*& s > 0 &*& arraySize |-> ?a;
-        //@ ensures set();//set(take(length(elems) - 1, elems));
+        //@ requires set(?elems) &*& size |-> ?s;
+        //@ ensures set(elems) &*& set(take(length(elems) - 1, elems));
         {
         
-        //@ open set();
-        for(int i = 0; i < elements.length; i++)
-        //@ invariant array_slice(elements, 0, arraySize, _) &*& i <= 0;
+        boolean update = false;
+        
+        //@ open set(elems);
+        for(int i = 0; i < size-1; i++)
+        //@ invariant array_slice(elements, 0, size, _) &*& i <= 0;
         {
         	if(elements[i] == o){
-        		elements[i] = null;
+        		update = true;
+        	}
+        	if(update){
+        		elements[i] = elements[i+1];
         	}
         }
         size--;
-        //@ close set();
+        //@ close set(elems);
     }
 
 }
